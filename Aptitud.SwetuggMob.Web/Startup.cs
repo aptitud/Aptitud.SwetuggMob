d8cb2dbe895e48f6526ba208c9db589705b98126
Gustav Dahlman <dahlman76@gmail.com>
2015-01-31 00:15:50 +0100

    trying to get apphb to find startup class

--- Aptitud.SwetuggMob.Web/Startup.cs
+++ Aptitud.SwetuggMob.Web/Startup.cs
@@ -3,16 +3,14 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartup(typeof(Aptitud.SwetuggMob.Web.Startup))]
+[assembly: OwinStartup(typeof(Startup))]
 
-namespace Aptitud.SwetuggMob.Web
+public class Startup
 {
-    public class Startup
+    public void Configuration(IAppBuilder app)
     {
-        public void Configuration(IAppBuilder app)
-        {
-            app.UseNancy();
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-        }
+        app.UseNancy();
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
     }
 }
+
