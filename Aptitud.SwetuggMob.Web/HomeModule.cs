0d6f577c35f32844063d14f5885200d1b84cded1
Hampus Klarin <hampus@gmail.com>
2015-01-30 13:52:17 +0100

    Added start page

---
+++ Aptitud.SwetuggMob.Web/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Aptitud.SwetuggMob.Web
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule() {
+            Get["/"] = parameters => "Hello Swetugg";
+        }
+    }
+}
