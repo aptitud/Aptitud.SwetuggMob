96d9452919778cd0c4be31fd2339b5a7a861aeb7
David Blomberg <david.blomberg@gmail.com>
2015-02-02 11:05:40 +0100

    Added tests

---
+++ Aptitud.SwetuggMob.Web/Services/TwitterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aptitud.SwetuggMob.Web.Services
+{
+    public class TwitterService
+    {
+    }
+}
