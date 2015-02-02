161ef803ca012e505a4ff7ffae7dc556d699c342
Gustav Dahlman <dahlman76@gmail.com>
2015-02-02 12:10:05 +0100

    HomeView

--- Aptitud.SwetuggMob.Web/HomeModule.cs
+++ Aptitud.SwetuggMob.Web/HomeModule.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Nancy;
+using Aptitud.SwetuggMob.Web.Views;
 
 namespace Aptitud.SwetuggMob.Web
 {
     public class HomeModule : NancyModule
     {
-        public HomeModule() {
-            Get["/"] = parameters => "Hello Swetugg";
+        public HomeModule()
+        {
+
+            var model = new HomeViewModel
+            {
+                Tweets = new HomeViewModel.TweetItem[]
+                {
+                  new HomeViewModel.TweetItem{
+                      Message = "Hello"
+                  },
+                  new HomeViewModel.TweetItem{
+                      Message = "Swetugg"
+                    }
+                }
+            };
+
+
+            Get["/"] = parameters => View["Home", model];
         }
     }
 }
