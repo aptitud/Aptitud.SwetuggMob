6517938f4a7454cf6282e48166bd32f17a9b8664
David Blomberg <david.blomberg@gmail.com>
2015-02-02 15:43:15 +0100

    Get user.location from tweet

--- Aptitud.SwetuggMob.Web/Services/TweetFinder.cs
+++ Aptitud.SwetuggMob.Web/Services/TweetFinder.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using Tweetinvi;
+using Tweetinvi.Core.Events;
 using Tweetinvi.Core.Interfaces;
 
 namespace Aptitud.SwetuggMob.Web.Services
@@ -15,10 +17,15 @@ namespace Aptitud.SwetuggMob.Web.Services
             var startup = Startup.Value;
         }
 
-
         public IEnumerable<ITweet> GetTweetsForHashTag(string hashtag)
         {
             return Search.SearchTweets(hashtag);
+        }
+
+        public IEnumerable<string> GetDistinctLocationsFromTweets(IEnumerable<ITweet> tweets)
+        {
+            return tweets.Where(t => !String.IsNullOrEmpty(t.Creator.Location))
+                .Select(tweet => tweet.Creator.Location).ToList().Distinct();
         }
     }
 
--- Tests/FetchTweetsTests.cs
+++ Tests/FetchTweetsTests.cs
@@ -1,18 +1,37 @@
-﻿using Aptitud.SwetuggMob.Web.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aptitud.SwetuggMob.Web.Services;
 using NUnit.Framework;
+using Tweetinvi.Core.Interfaces;
 
 namespace Tests
 {
     [TestFixture]
     public class FetchTweetsTests
     {
-        [Test]
+        [Test, Ignore]
         public void FetchTweetsTest()
         {
-            var finder = new TweetFinder();
-            var tweets = finder.GetTweetsForHashTag("#Swetugg");
+            var tweets = GetSwetuggTweets();
             Assert.That(tweets, Is.Not.Null);
             Assert.That(tweets, Is.Not.Empty);
         }
+
+        private static IEnumerable<ITweet> GetSwetuggTweets()
+        {
+            var finder = new TweetFinder();
+            var tweets = finder.GetTweetsForHashTag("#Swetugg");
+            return tweets;
+        }
+
+        [Test,Ignore]
+        public void GetLocationsFromTweets()
+        {
+            var finder = new TweetFinder();
+            var tweets = finder.GetTweetsForHashTag("#Swetugg");
+            var locations = finder.GetDistinctLocationsFromTweets(tweets);
+        }
+
     }
 }
